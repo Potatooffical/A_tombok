416af49f806aa896e1cd04c85f900fd1df95b6d0
Potatooffical <danorb12@gmail.com>
2025-02-11 10:06:47 +0100

    Add project files.

---
+++ Feladat_jegyzet_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Feladat_jegyzet_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random szamgen = new Random();
+            int[] veletlenszam = new int[10];
+            for (int i = 0; i < veletlenszam.Length; i++)
+            {
+                veletlenszam[i] = szamgen.Next(1,101);
+                
+            }
+            foreach (int item in veletlenszam)
+                {
+                    Console.Write($"{item} | ");
+                }
+            Console.WriteLine("Pogram vége");
+            Console.ReadLine();
+        }
+    }
+}
